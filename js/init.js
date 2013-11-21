76f0856ea9cf3bf232814f7a2bb6f6e8b38668f4
vladsv <super.vladsv@gmail.com>
2013-11-21 19:23:10 +0200

    Made task

---
+++ js/init.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('.slider').mobilyslider({
+		transition: 'fade',
+		animationSpeed: 300,
+		bullets: true,
+		arrowsHide: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
+		bullets: false
+	});
+	
+	
+});
